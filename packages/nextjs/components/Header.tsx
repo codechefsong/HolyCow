5a4cbad93eb04b3d588566f5354f98edc2beb3a8
Codechef Song <133816663+codechefsong@users.noreply.github.com>
2024-12-08 21:57:43 -0500

    add cow profile page

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -4,7 +4,7 @@ import React, { useCallback, useRef, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { Bars3Icon, BugAntIcon, CreditCardIcon, PuzzlePieceIcon } from "@heroicons/react/24/outline";
+import { Bars3Icon, BugAntIcon, CreditCardIcon, PuzzlePieceIcon, WrenchIcon } from "@heroicons/react/24/outline";
 import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
 import { useOutsideClick } from "~~/hooks/scaffold-eth";
 
@@ -18,6 +18,11 @@ export const menuLinks: HeaderMenuLink[] = [
   {
     label: "Home",
     href: "/",
+  },
+  {
+    label: "Your Cow",
+    href: "/cowprofile",
+    icon: <WrenchIcon className="h-4 w-4" />,
   },
   {
     label: "Buy Cow",
